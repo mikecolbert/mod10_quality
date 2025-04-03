d5d5b22978182a8d1ed25610e42e440566e6a830
mikecolbert <michael-colbert@uiowa.edu>
2025-04-03 14:36:32 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import Flask
+"""
+This is a simple Flask application that returns "Hello, World!" when accessed at the root URL.
+"""
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    """Return a greeting message."""
     return "Hello, World!"
 
 
